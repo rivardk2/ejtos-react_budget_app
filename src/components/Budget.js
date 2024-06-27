e9c449b9cb5c0debca71e27f5743c778e1efc0e1
Kim Rivard <rivardk2@winthropalumni.com>
2024-06-27 11:40:49 -0400

    testing commits

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,4 +1,4 @@
 //adding text and value for your budget
 //Will be importing app context and the useContext hook and pass AppContext to it 
 //(this is how a component connects to the context in order to get values from global state)
-
+//test
